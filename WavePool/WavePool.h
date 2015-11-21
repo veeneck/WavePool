d00e0b191d5a86b568acfbd26c716b32abc255e3
Ryan Campbell <veeneck@gmail.com>
2015-11-21 13:21:22 -0500

    Initial Commit

---
+++ WavePool/WavePool.h
@@ -0,0 +1,19 @@
+//
+//  WavePool.h
+//  WavePool
+//
+//  Created by Ryan Campbell on 11/21/15.
+//  Copyright © 2015 Phalanx Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WavePool.
+FOUNDATION_EXPORT double WavePoolVersionNumber;
+
+//! Project version string for WavePool.
+FOUNDATION_EXPORT const unsigned char WavePoolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WavePool/PublicHeader.h>
+
+
